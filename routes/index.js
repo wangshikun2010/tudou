0e08986c2251be645a405a6cebbdefa618fcc058
wangshikun2010@gmail.com <wangshikun2010>
2015-09-14 20:50:49 +0800

    修改首页

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,29 @@ var mongoose = require('mongoose');
 router.get('/', function(req, res, next) {
     console.log('request home page');
 
-    // 定位到首页
-    res.render('news', {
+    // 首页
+    res.render('index', {
+        title: '土豆装修工作室',
+        data: [
+            {
+                id: 1,
+                content: ''
+            },
+            {
+                id: 2,
+                content: ''
+            }
+        ]
+    });
+
+});
+
+// 装修案例
+router.get('/show', function(req, res, next) {
+    console.log('request show page');
+
+    // 首页
+    res.render('show', {
         title: '挺好的',
         data: [
             {
@@ -43,62 +64,6 @@ router.get('/about', function(req, res, next) {
             }
         ]
     });
-
-
-    // // 连接数据库
-    // var db = mongoose.connect('mongodb://@localhost:10001/test').connection;
-
-    // // 打开
-    // db.once('open', function() {
-    //     console.log("mongodb connection open");
-    // });
-
-    // // 错误
-    // db.on('error', function(error) {
-    //     console.log(error);
-    //     console.log('错误');
-    // });
-
-    // // Schema 结构
-    // var mongooseSchema = new mongoose.Schema({
-    //     username : {type : String, default : '匿名用户'},
-    //     title    : {type : String},
-    //     content  : {type : String},
-    //     time     : {type : Date, default: Date.now},
-    //     age      : {type : Number}
-    // });
-
-    // // 添加 mongoose 实例方法
-    // mongooseSchema.methods.findbyusername = function(username, callback) {
-    //     return this.model('mongoose').find({username: username}, callback);
-    // }
-
-    // // 添加 mongoose 静态方法，静态方法在Model层就能使用
-    // mongooseSchema.statics.findbytitle = function(title, callback) {
-    //     return this.model('mongoose').find({title: title}, callback);
-    // }
-
-    // // model
-    // var mongooseModel = db.model('mongoose', mongooseSchema);
-
-    // 增加记录 基于 entity 操作
-    // var doc = {
-    //     username : 'wangshikun',
-    //     title : '王仕昆',
-    //     content : '前端开发工程师',
-    //     age: 20
-    // };
-    // var mongooseEntity = new mongooseModel(doc);
-    // mongooseEntity.save(function(error) {
-    //     if(error) {
-    //         console.log('保存失败');
-    //     } else {
-    //         console.log('保存成功');
-    //     }
-    //     // 关闭数据库链接
-    //     db.close();
-    // });
-
 });
 
 module.exports = router;
